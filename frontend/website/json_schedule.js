f8639c439e98a8aaeb65a02e14a4ade2e2642a10
vxu0 <vivianx2@illinois.edu>
2023-03-23 23:43:37 -0500

    edited schedule json input function (still troubleshooting)

--- frontend/website/json_schedule.js
+++ frontend/website/json_schedule.js
@@ -1,16 +1,16 @@
-const data = { schedule: "" };
-
-fetch('./JSON/data.json', {
-    method: "POST", // or 'PUT'
+// const data = { schedule: "1" };
+fetch('JSON/example_schedule.json', {
+    method: "GET", // or 'PUT'
     headers: {
-        "Content-Type": "application/json",
+        "Accept": "application/json",
     },
-    body: JSON.stringify(data),
+    // body: JSON.stringify(data),
 })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log("Success:", data);
-    })
-    .catch((error) => {
-        console.error("Error:", error);
-    });
+.then(response => response.json())
+.then(json => console.log(json));
+    // .then((data) => {
+    //     console.log("Success:", data);
+    // })
+    // .catch((error) => {
+    //     console.error("Error:", error);
+    // });
