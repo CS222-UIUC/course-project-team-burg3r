529499423419ecae10d3bedc72318cd2b745b2b6
vxu0 <vivianx2@illinois.edu>
2023-03-23 23:42:03 -0500

    added functions for adding course inputs and submitting all courses

--- frontend/website/script.js
+++ frontend/website/script.js
@@ -2,6 +2,23 @@ function menuToggle() {
     document.getElementById("dropdown").classList.toggle("show");
 }
 
+function addCourse() {
+    var course = {
+        subject: document.getElementById("subject").value,
+        number: document.getElementById("number").value
+    };
+            
+    // currently just displays the value added (to change)
+    alert(JSON.stringify(course));
+}
+
+function submitCourses() {
+    var courses = document.getElementsByClassName("course")
+
+    // currently just displays all courses added (to change)
+    alert(JSON.stringify(courses));
+}
+
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
