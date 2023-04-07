2aa0dc9909230f030ec7f48c4af70ce4e9507674
craigsterr <102565602+craigsterr@users.noreply.github.com>
2023-04-06 23:46:05 -0500

    weather page now displays current uiuc weather

--- frontend/website/script.js
+++ frontend/website/script.js
@@ -58,26 +58,26 @@ let weather = {
       .then((response) => response.json())
       .then((data) => this.displayWeather(data));
     },
+    fetchWeather2: function () {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q="
+        + "Champaign"
+        + "&units=imperial&appid=463a026d4300064311c0be34302d7697"
+      )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+    },
     displayWeather : function(data) {
       const { name } = data;
       const { icon, description } = data.weather[0];
       const { temp, humidity } = data.main;
       const { speed } = data.wind;
       console.log(name, icon, description, temp, humidity, speed)
-      document.querySelector(".city").innerText = "Weather in " + name;
-      document.querySelector(".icon").src = "https://openweathermap.org/img/wn" + icon + "@2x.png";
+      // document.querySelector(".city").innerText = "Weather in " + name;
       document.querySelector(".description").innerText = description;
       document.querySelector(".temp").innerText = temp + "°F";
       document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
       document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
-  },
-  search: function() {
-    this.fetchWeather(document.querySelector(".search-bar").value);
   }
 };
 
-document
-  .querySelector(".search button")
-  .addEventListener("click", function() {
-    weather.search();
-})
