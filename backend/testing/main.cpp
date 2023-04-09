fa70b7cf2aa68315807a41e41fdc78dc6e0f1ce5
Mr-Sheep <24599209+Mr-Sheep@users.noreply.github.com>
2023-04-09 04:40:13 -0500

    simple api w/ nodejs

--- backend/testing/main.cpp
+++ backend/testing/main.cpp
@@ -7,15 +7,22 @@
 #include "schedule_maker.h"
 
 
-int main() {
+int main(int argc, char *argv[]) {
+	if (argc < 3) {
+		std::cerr << "Usage: " << argv[0] << " <input_file>" << " <time_stamp>" << std::endl;
+		return 1;
+	}
+
 	// Parse the json file with course info and required courses
-	read_courses("example/required.json", "example/all_course.json");
+	read_courses(argv[1], "example/all_course.json");
 
 	// Print out the stats of how many courses/sections were read
 	read_stats();
 
+	std::string time_stamp = argv[2];
+	std::string output = "example/output-" + time_stamp + ".json";
 	// Output one json holding all schedules
-	write_courses("example/output.json");
+	write_courses(output);
 
 	// Output one json for every schedule
 	write_courses_individual("example/outputs/out");
