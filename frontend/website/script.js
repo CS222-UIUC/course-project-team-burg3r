1922bf8692e6a200dc216037c2afae091708c50a
vxu0 <vivianx2@illinois.edu>
2023-05-03 16:40:35 -0500

    moved weather js to separate file

--- frontend/website/script.js
+++ frontend/website/script.js
@@ -209,37 +209,5 @@ window.onclick = function(event) {
     }
 }
 
-let weather = {
-    "apiKey": "463a026d4300064311c0be34302d7697",
-    fetchWeather: function (city) {
-      fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q="
-        + city
-        + "&units=imperial&appid=463a026d4300064311c0be34302d7697"
-      )
-      .then((response) => response.json())
-      .then((data) => this.displayWeather(data));
-    },
-    fetchWeather2: function () {
-      fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q="
-        + "Champaign"
-        + "&units=imperial&appid=463a026d4300064311c0be34302d7697"
-      )
-      .then((response) => response.json())
-      .then((data) => this.displayWeather(data));
-    },
-    displayWeather : function(data) {
-      const { name } = data;
-      const { icon, description } = data.weather[0];
-      const { temp, humidity } = data.main;
-      const { speed } = data.wind;
-      console.log(name, icon, description, temp, humidity, speed)
-      // document.querySelector(".city").innerText = "Weather in " + name;
-      document.querySelector(".description").innerText = description;
-      document.querySelector(".temp").innerText = temp + "°F";
-      document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-      document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
-  }
-};
+
 
